4271e494cc35714def728bf5584fba09eee76ed5
Hiromi Takahashi <hiromix222@gmail.com>
2022-12-09 20:04:17 +0900

    first commit

---
+++ src/main/java/com/example/jeedemo/JeedemoRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.jeedemo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class JeedemoRestApplication extends Application {
+}
